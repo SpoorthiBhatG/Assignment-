437a9cbe250d90ef94c469eee939ffc8578e21a2
Spoorthi Bhat G <spoorthibhat.27@gmail.com>
2024-12-19 13:47:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('#registrationForm').on('submit', function (event) {
-        event.preventDefault(); // Prevent default form submission
-
-        const formData = $(this).serialize(); // Serialize the form data
-        window.location.href = 'complete.html?' + formData; // Redirect to the Complete page
-    });
-});
